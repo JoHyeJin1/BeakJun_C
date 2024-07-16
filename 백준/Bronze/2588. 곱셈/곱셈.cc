bec5ea282a4e5bfed6c68c113c66660e354cca55
JoHyeJin <121273234+JoHyeJin1@users.noreply.github.com>
2024-07-17 00:57:22 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d\n", a * (b % 10));
+    printf("%d\n", a * (b / 10 % 10));
+    printf("%d\n", a * (b / 100));
+    printf("%d", a * b);
+}
